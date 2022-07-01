ff4e7b02377c4cbe54471ac9a49951a65fddf92b
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-01 13:42:12 -0500

    Update scene.js

--- designer/scene.js
+++ designer/scene.js
@@ -3,8 +3,6 @@
 const OBJLoader = new THREE.OBJLoader();
 const imageLoader = new THREE.ImageLoader();
 const canvas = document.querySelector('#threejs-canvas');
-const width = window.innerWidth;
-const height = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({canvas});
