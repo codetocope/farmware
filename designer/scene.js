3c31583f961b0eeb3f5745e41b9707998602e1db
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-02 19:52:05 -0500

    Update scene.js
    testing sprite removal

--- designer/scene.js
+++ designer/scene.js
@@ -39,9 +39,10 @@ texloader.load('/images/colortesting.png',
     handsprite.scale.set(10, 10, 1);
     scene.add(handsprite);
     handsprite.position.set(0, 5, -10);
-    handsprite.transparent = true;
   }
 );
+
+scene.remove(handsprite);
 
 scene.add(ambLight);
 scene.add(light);
