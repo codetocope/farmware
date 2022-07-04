ed07b97eb4e04aa9e7231329910bdea67e6daa14
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-04 11:01:13 -0500

    Update scene.js
    fixed line spacing

--- designer/scene.js
+++ designer/scene.js
@@ -1,31 +1,41 @@
 /* NOTE ON OBJ: Make sure no n-sided polygons! OBJ can recognize these as faces, but the THREE.js importer has trouble with these. */
 const OBJLoader = new THREE.OBJLoader();
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
 var ringSelectedInset = null;
+
 const ambLight = new THREE.AmbientLight(0xffffff, .35);
 const skyColor = 0xB1E1FF; // light blue  // TODO: Change color
 const groundColor = 0xB97A20; // brownish orange
+
 const materialBlack = new THREE.Color(0x303030);
 const materialWhite = new THREE.Color(0xdbdbdb);
 const materialOrange = new THREE.Color(0xfc7b03);
+
 var loadedInsets = [];
 var currInsetIndex = 0;
+
 const light = new THREE.PointLight(0xffffff, 0.9, 18);
 light.position.set(0, 6, 0);
 light.castShadow = true;
+
 var currentType = "hand";
+
 var texloader = new THREE.TextureLoader();
 var wristLoader = new THREE.TextureLoader(); 
 var bodyLoader = new THREE.TextureLoader(); //not sure this is necessary but just in case;
+
 var handPhoto,
     handsprite,
     armPhoto,
     armsprite,
     bodyPhoto,
     bodysprite;
+
 /*texloader.load('/images/hand.png', 
   function(tex) {
     handPhoto = new THREE.SpriteMaterial( { map: tex, color: 0xffffff } );
@@ -37,8 +47,10 @@ var handPhoto,
     handPhoto.opacity = .25;
   }
 ); */
+
 //handPhoto.opacity = .25; // Note: Trying to change opacity outside of specifically the loader breaks the website
 //If I instead try to change the loader, as follows:
+
 const map = new THREE.TextureLoader().load( '/images/hand.png' );
 const testmaterial = new THREE.SpriteMaterial( { map: map, transparent: true } );
 
@@ -48,49 +60,69 @@ testmaterial.opacity = .2;
 
 scene.add(ambLight);
 scene.add(light);
+
 // Loads ring by default, does so here 
 document.addEventListener("keydown", onDocumentKeyDown, false);
+
 loadOBJ('ringBase');
+
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(50, 50, 10, 10),
     new THREE.MeshPhongMaterial({
         color: 0xffffff
     })
 );
+
 plane.rotation.x = Math.PI * -.5;
 plane.position.y = -1;
+
 const defaultMaterial = new THREE.MeshLambertMaterial({
     color: 0xffffff,
     wireframe: false,
 });
+
 camera.position.z = 7;
 camera.position.y = 3;
+
 var group = new THREE.Object3D();
 scene.add(group);
+
 keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 keyLight.position.set(-100, 0, 100);
+
 fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
 fillLight.position.set(100, 0, 100);
+
 backLight = new THREE.DirectionalLight(0xffffff, 1.0);
 backLight.position.set(100, 0, -100).normalize();
+
 scene.add(backLight);
+
 const gridHelper = new THREE.GridHelper(36, 1);
 scene.add(gridHelper);
+
 renderer.setSize(window.innerWidth - 15, window.innerHeight - 15);
 renderer.setClearColor(0x242d45, 1);
 document.body.appendChild(renderer.domElement);
+
 function loadOBJ(objName) {
+    
     var manager = new THREE.LoadingManager();
     manager.onProgress = function (item, loaded, total) {
+        
         //console.log(item, loaded, total);
     };
+    
     // model
     var mesh;
     var loader = new THREE.OBJLoader(manager);
     loader.load('/obj/individualComponents/' + objName + '.obj', function (object) {
         //console.log(object);
+        
         object.traverse(function (child) {
+            
             if (child instanceof THREE.Mesh) {
+                
                 child.material = defaultMaterial;
                 child.material.side = THREE.DoubleSide;  // could not find this in the Three library 
                 var geometry = new THREE.Geometry().fromBufferGeometry(child.geometry);
@@ -99,15 +131,19 @@ function loadOBJ(objName) {
                 mesh.name = objName;
                 mesh.castShadow = true;
                 //mesh.material.color.set(materialOrange);
+                
                 if (objOptions.type == 'Ring' && mesh.name != 'ringBase') {
                     mesh.rotation.x = Math.PI * -0.5;
                     mesh.rotation.z = Math.PI;
+                    
                     mesh.position.y = 1.1;
                     mesh.position.z = 1.69;
+                    
                     loadedInsets[0] = mesh;
+                    
                 } else if (objOptions.type.indexOf('Bracelet') > -1 && mesh.name != 'braceletBase') { // Why the != here?
                     loadedInsets[currInsetIndex] = mesh; // Why is this different?
-                    // Because not all bracelets have insets? Test this whilst adding a similar object
+                    
                     if (mesh.name == "waveInset") {
                         mesh.position.x = 0;
                         mesh.position.y = 1 - mesh.scale.z;
@@ -125,33 +161,45 @@ function loadOBJ(objName) {
         });
     });
 }
+
 function saveFile(fileName) {
+    
     const exportSTL = new THREE.STLExporter();
+    
     var newGeo = new THREE.Geometry();
+    
     for (var c in group.children) {
         group.children[c].updateMatrix();
         newGeo.merge(group.children[c].geometry, group.children[c].matrix);
     }
     var exportItem = new THREE.Mesh(newGeo, defaultMaterial);
+    
     var stlString = exportSTL.parse(exportItem);
     //console.log(group);
+    
     const blob = new Blob([stlString], {
         type: exportSTL.mimeType
     });
+    
     if (fileName.trim() != '')
         saveAs(blob, fileName + '.stl');
     else
         saveAs(blob, 'default.stl');
 }
 function updateModel(baseName) {
+    
     while (group.children.length) {
         group.remove(group.children[0]);
     }
+    
     loadOBJ(baseName);
     //console.log(group);
     scene.add(group);
+    
 }
+
 function updateModelOptions(opts, multiFlag = false, clearAll = false) {
+    
     // TODO: find better solution for clearing old options than clearing and loading everytime
     if (clearAll) {
         while (group.children.length) {
@@ -164,6 +212,7 @@ function updateModelOptions(opts, multiFlag = false, clearAll = false) {
             loadedInsets = [];
         }
     }
+    
     if (!multiFlag) { // check if options is an array i.e. there are multiple insets
         currInsetIndex = 0;
         loadOBJ(opts + 'Inset');
@@ -177,10 +226,12 @@ function updateModelOptions(opts, multiFlag = false, clearAll = false) {
         }
     }
 }
+
 function setColor(color){
     
     for (var c in group.children) {
         var m = group.children[c];
+        
         if (color == 'white')
             m.material.color.set(materialWhite);
         else if(color == 'orange')
@@ -192,8 +243,10 @@ function setColor(color){
 function setPreview (type, show = false) {
     // get preview images
     // switch camera perspective (straight on?)
+    
     var currCameraRot = new THREE.Vector3();
     camera.getWorldDirection(currCameraRot);
+    
     if (show) {
         camera.rotation.x = 0;
     } else {
@@ -206,6 +259,7 @@ function setInsetScale(meshIndex, newValue, axis = 0) {
     else
         loadedInsets[meshIndex].scale[axis] = newValue;
 }
+
 function windowResized() {
     const width = window.innerWidth;
     const height = window.innerHeight;
@@ -214,6 +268,7 @@ function windowResized() {
     renderer.setSize(width, height);
     setCanvasDimensions(renderer.domElement, width, height);
 }
+
 function onDocumentKeyDown(event) {
     var keyCode = event.key;
     
