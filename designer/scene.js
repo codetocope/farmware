108d4a40f4e50298a2caade90b3df30457958029
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-04 10:35:12 -0500

    Update scene.js

--- designer/scene.js
+++ designer/scene.js
@@ -44,8 +44,6 @@ texloader.load('/images/hand.png',
     handsprite.scale.set(10, 10, 1);
     scene.add(handsprite);
     handsprite.position.set(0, 5, -10);
-    handPhoto.transparency = true;
-    handPhoto.opacity = .25;
   }
 ); 
 
