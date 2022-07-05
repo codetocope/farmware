0002b7c83f097eb914f9c001cbd159645bdedc06
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-05 13:03:47 -0500

    Update scene.js

--- designer/scene.js
+++ designer/scene.js
@@ -54,7 +54,7 @@ wristLoader.load('/images/arm.png',
   function(wri) {
     armPhoto = new THREE.SpriteMaterial( { map: wri, color: 0xffffff, rotation: Math.PI / 5 } );
     armsprite = new THREE.Sprite( armPhoto );
-    armsprite.scale.set(30, 25, 1);
+    armsprite.scale.set(35, 28, 1);
     scene.add(armsprite);
     armsprite.position.set(0, -0.5, -4);
     armPhoto.opacity = 0;
