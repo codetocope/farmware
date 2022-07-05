cfb51b7a5004a8cc789c4ae5f0a10619e3b9749a
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2022-07-05 11:16:52 -0500

    Update scene.js

--- designer/scene.js
+++ designer/scene.js
@@ -52,7 +52,7 @@ texloader.load('/images/hand.png',
 
 wristLoader.load('/images/arm.png', 
   function(wri) {
-    armPhoto = new THREE.SpriteMaterial( { map: wri, color: 0xffffff } );
+    armPhoto = new THREE.SpriteMaterial( { map: wri, color: 0xffffff, rotation: Math.PI / 6 } );
     armsprite = new THREE.Sprite( armPhoto );
     armsprite.scale.set(25, 20, 1);
     scene.add(armsprite);
